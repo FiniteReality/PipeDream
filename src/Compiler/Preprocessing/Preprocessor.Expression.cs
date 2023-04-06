3502dd8d976bdc358b0e2f972b55441dbb7df405
FiniteReality <digpoealternate@gmail.com>
2023-04-06 07:57:39 +0100

    Delete the preprocessor in preparation of rewrite

--- src/Compiler/Preprocessing/Preprocessor.Expression.cs
+++
@@ -1,9 +0,0 @@
-using PipeDream.Compiler.Parsing.Tree;
-
-namespace PipeDream.Compiler.Preprocessing;
-
-public partial struct Preprocessor
-{
-    private ExpressionNode? HandleExpression(ExpressionNode expression)
-        => expression;
-}
